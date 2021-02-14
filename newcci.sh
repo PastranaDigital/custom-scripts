6b6cf72b1d1df4dbd1127cae9926f5b294c2f505
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-02-14 09:11:23 -0600

    added default values for common inputs

--- newcci.sh
+++ newcci.sh
@@ -5,26 +5,41 @@
 # cd desktop/custom-scripts/
 # newcci.sh
 
-echo "Enter Project Details -----------------------------------------------------------------------"
+echo "New CumulusCI Project -----------------------------------------------------------------------"
+read -p 'Dev Hub Org Alias or [CCIDevHub]: ' devhuborgalias
+if [[ $devhuborgalias == "" ]]; then
+    devhuborgalias="CCIDevHub"
+fi
+echo "Dev Hub Org Alias Name: $devhuborgalias"
+
 read -p 'Git URL: ' giturl
 projectname=$(echo $(echo ${giturl##*/}) | cut -d'.' -f 1)
 echo 'Project Name: ' $projectname
-read -p 'New Branch Name (no spaces): ' branchname
-read -p 'Dev Hub Org Alias: ' devhuborgalias
+read -p 'New Branch Name [dev]: ' branchname
+if [[ $branchname == "" ]]; then
+    branchname="dev"
+fi
+echo "New Branch Name: $branchname"
 
 echo "Git: Cloning an Existing Repository ---------------------------------------------------------"
 cd /g/Google_Drive/GitHub/
 git clone $giturl $projectname
-cd $projectname
-cd ..
+#cd $projectname
+#cd ..
 
-#echo "Git: Creating New Branch [feature/$branchname] ------------------------------------------------------"
-#git branch feature/$branchname
-#git switch feature/$branchname
-#git add .
-#git commit -m 'init'
-#git push -u origin feature/$branchname
-#git log
+echo "CumulusCI: Initializing Project -------------------------------------------------------------"
+cci project init
+echo "CumulusCI: Connecting Service ---------------------------------------------------------------"
+sfdx force:config:set defaultdevhubusername=$devhuborgalias
+#cci service connect devhub --project
+
+echo "Git: Creating New Branch [feature/$branchname] ------------------------------------------------------"
+git branch feature/$branchname
+git switch feature/$branchname
+git add .
+git commit -m 'Set up Project'
+git push -u origin feature/$branchname
+git log
 
 #echo "SFDX: Authorizing Default Dev Hub Org -------------------------------------------------------"
 #sfdx force:auth:web:login -d -a $devhuborgalias -s
