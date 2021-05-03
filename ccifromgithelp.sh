8ffd284dcca3f68a1a8ac19662a8fdc59191bf34
Omar Pastrana <opastrana@salesforce.com>
2021-05-03 10:42:36 -0500

    new file

---
+++ ccifromgithelp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------"
+echo "+ PREWORK:                                              +"
+echo "+ 1) Created git repo in GitHub                         +"
+echo "+ 2) Folder you created is opened in VSCode             +"
+echo "---------------------------------------------------------"
